dd4a3f5713227bc0a572ab3350a68ab4c9265851
Denis Kaynar <kaynar.denis@gmail.com>
2022-12-12 01:08:14 +0300

    Istio added

--- install.sh
+++ install.sh
@@ -6,7 +6,5 @@ ROOT_RELEASE=root
 
 helm upgrade --install $ARGOCD_RELEASE argo-cd --create-namespace --namespace $NAMESPACE --repo https://argoproj.github.io/argo-helm
 
-kubectl delete secret sh.helm.release.v1.$ARGOCD_RELEASE.v1 --namespace $NAMESPACE
-
 helm upgrade --install $ROOT_RELEASE ./charts/root -n $NAMESPACE --repo https://argoproj.github.io/argo-helm
 
