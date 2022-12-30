6c2ee009958db2f62ef25c11a8fcbd6c763fdba8
Denis Kaynar <kaynar.denis@gmail.com>
2022-12-30 13:35:13 +0300

    Metallb and mix added

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ ROOT_RELEASE=root
 
 helm upgrade --install $ARGOCD_RELEASE argo-cd --create-namespace --namespace $NAMESPACE --repo https://argoproj.github.io/argo-helm
 
-helm upgrade --install $ROOT_RELEASE ./charts/root -n $NAMESPACE --repo https://argoproj.github.io/argo-helm
+helm upgrade --install $ROOT_RELEASE ./charts/root -n $NAMESPACE
 
